02a77c5d7ea6aaf97968de57d412ecb56a457277
Krasnonos <andreykrasnonos@icloud.com>
2022-08-07 12:52:20 +0300

    finish setup

---
+++ src/theme.js
@@ -0,0 +1,29 @@
+export const theme = {
+  colors: {
+    primary: '#F4E041',
+    secondary: '#00BDD3',
+    background: '#F8F8F8',
+    btnHover: '#FFE302',
+    btnDisabled: 'B4B4B4',
+    mainText: '#000000, 0.87',
+    secondaryText: '#FFFFFF',
+  },
+  space: [0, 2, 4, 8, 16, 32, 64, 128, 256],
+  fontSizes: {
+    s: '16px',
+    m: '40px',
+  },
+  fontWeights: {
+    normal: 400,
+    bold: 500,
+  },
+  borders: {
+    none: 'none',
+    normal: '1px solid',
+  },
+  radii: {
+    none: '0',
+    normal: '10px',
+    round: '20px',
+  },
+};
