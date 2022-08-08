a7309ebe6cd8d61a9054698a59b1e30c0b80e75b
Krasnonos <andreykrasnonos@icloud.com>
2022-08-08 16:40:29 +0300

    finish logic

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -1,20 +1,19 @@
-import { Formik, Form, Field } from 'formik';
 import { toast } from 'react-toastify';
 import { getToken } from '../../utils/getToken';
 import { postUser } from '../../utils/postUser';
-// import { getPosition } from '../../utils/getPosinions';
-// import { useState, useEffect } from 'react';
+import { getPosition } from '../../utils/getPosinions';
+import { useState, useEffect } from 'react';
 
 export const SignUpForm = ({ setIsNewUser }) => {
-  // const [positions, setPositions] = useState([]);
-  // useEffect(() => {
-  //   getPosition().then(setPositions);
-  // }, []);
+  const [positions, setPositions] = useState([]);
+  useEffect(() => {
+    getPosition().then(setPositions);
+  }, []);
 
-  const createPostBody = async (
-    { email, name, phone, position },
-    resetForm
-  ) => {
+  const createPostBody = async e => {
+    e.preventDefault();
+    const { position, name, email, phone } = e.target.elements;
+
     const inputFile = document.querySelector('input[type="file"]');
     const file = inputFile.files[0];
 
@@ -24,74 +23,59 @@ export const SignUpForm = ({ setIsNewUser }) => {
     }
 
     let formData = new FormData();
-    formData.append('position_id', position);
-    formData.append('name', name);
-    formData.append('email', email);
-    formData.append('phone', phone);
+    formData.append('position_id', position.value);
+    formData.append('name', name.value);
+    formData.append('email', email.value);
+    formData.append('phone', phone.value);
     formData.append('photo', file);
 
     const { token } = await getToken();
     const { success } = await postUser(formData, token);
     if (success) {
       setIsNewUser(true);
-      resetForm();
+      e.target.reset();
     }
   };
 
   return (
-    <Formik
-      initialValues={{
-        email: '',
-        name: '',
-        phone: '',
-        position: '1',
-        photo: '',
-      }}
-      onSubmit={(values, { resetForm }) => {
-        createPostBody(values, resetForm);
-      }}
-    >
-      <Form>
-        <Field
-          type="text"
-          name="name"
-          placeholder="Your Name"
+    <form onSubmit={createPostBody}>
+      <input
+        type="text"
+        name="name"
+        placeholder="Your Name"
+        required
+        minLength="2"
+        maxLength="60"
+      />
+      <input type="email" name="email" placeholder="Email" required />
+      <label>
+        <input
+          type="tel"
+          name="phone"
+          placeholder="Phone"
           required
-          minLength="2"
-          maxLength="60"
+          pattern="^\+380\d{3}\d{2}\d{2}\d{2}$"
         />
-        <Field type="email" name="email" placeholder="Email" required />
-        <label>
-          <Field
-            type="tel"
-            name="phone"
-            placeholder="Phone"
-            required
-            pattern="^\+380\d{3}\d{2}\d{2}\d{2}$"
-          />
-          +38 (XXX) XXX - XX - XX
-        </label>
-        <p>Select your position</p>
-        <Field type="radio" name="position" value="1" id="frontend developer" />
-        <label htmlFor="frontend developer">Frontend developer</label>
-        <Field type="radio" name="position" value="2" id="backend developer" />
-        <label htmlFor="backend developer">Backend developer</label>
-        <Field type="radio" name="position" value="3" id="designer" />
-        <label htmlFor="designer">Designer</label>
-        <Field type="radio" name="position" value="4" id="qa" />
-        <label htmlFor="qa">QA</label>
+        +38 (XXX) XXX - XX - XX
+      </label>
+      <p>Select your position</p>
+      {positions.map(({ id, name }) => (
+        <div key={id}>
+          <input type="radio" name="position" value={id} id={name} />
+          <label htmlFor={name}>{name}</label>
+        </div>
+      ))}
 
-        <label>
-          <Field
-            type="file"
-            name="photo"
-            required
-            accept="image/jpeg,image/jpg"
-          />
-          Upload your photo
-        </label>
-        <button type="submit">Sign up</button>
-      </Form>
-    </Formik>
+      <label>
+        <input
+          type="file"
+          name="photo"
+          required
+          accept="image/jpeg,image/jpg"
+        />
+        Upload your photo
+      </label>
+      <button type="submit">Sign up</button>
+    </form>
   );
 };
